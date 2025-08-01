580c2d37f9b2bd021809e1a4c4aa1129f8c6a87c
Alanis, Adrian <ze13ju@gmail.com>
2025-08-01 17:19:58 +0200

    feat(jupyter): centos9 had to be used

--- docker-jupyter/jupyter_notebook_config.py
+++ docker-jupyter/jupyter_notebook_config.py
@@ -1,12 +1,12 @@
 ## Jupyter Lab Configuration ##
 
 # Security and Access ##
-c.NotebookApp.password = ''
-c.NotebookApp.token = ''
+c.NotebookApp.password = ""
+c.NotebookApp.token = ""
 
 c.NotebookApp.open_browser = False
-c.NotebookApp.ip = '*'
+c.NotebookApp.ip = "*"
 c.NotebookApp.port = 8888
 
 # The directory to use for notebooks and kernels
-c.NotebookApp.notebook_dir = '/workspace'
+c.NotebookApp.notebook_dir = "/workspace"
